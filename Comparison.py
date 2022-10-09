0c5b7595c6d79a55adfb60042aa5d2123e814e3b
Cauti0n 104 <hasya.lesmana21@gmail.com>
2022-10-09 14:26:38 +0700

    Operator,Logical,Comparison
    Operator,Logical,Comparison

---
+++ Comparison.py
@@ -0,0 +1,9 @@
+a = int(input("masukan bilangan satu :"))
+b = int(input("masukan bilangan dua :"))
+
+print(a < b) #False
+print(a <= b) #False
+print(a > b) #True
+print(a >= b) #True
+print(a == b) #False
+print(a != b) #True
